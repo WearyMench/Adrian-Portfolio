487bce2612e5bea90d1324b7156f6d8c5723429e
Adrian Mirabal <109903507+WearyMench@users.noreply.github.com>
2025-03-09 01:34:04 -0400

    Update socialLinks.js

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,34 +1,34 @@
 export const Data = [
   {
-    title: "LinkedIn: adrianm07",
+    title: "LinkedIn",
     url: "https://www.linkedin.com/in/adrianm07",
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png",
     alt: "LinkedIn Logo",
   },
   {
-    title: "Instagram: @adr_mir07",
-    url: "https://www.instagram.com/adr_mir07/",
+    title: "Instagram",
+    url: "https://www.instagram.com/",
     imagen:
       "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png",
     alt: "Instagram logo",
   },
   {
-    title: "Twitter: @adrMir07",
+    title: "Twitter",
     url: "https://twitter.com/adrMir07",
     imagen:
       "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png",
     alt: "Twitter Logo",
   },
   {
-    title: "Telegram: @Adr_0",
-    url: "https://t.me/adr_0",
+    title: "Telegram",
+    url: "https://t.me/",
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png",
     alt: "Telegram Logo",
   },
   {
-    title: "Discord: adr07#9601",
+    title: "Discord",
     url: "https://discord.com/",
     imagen:
       "https://logodownload.org/wp-content/uploads/2017/11/discord-logo-1-1.png",
